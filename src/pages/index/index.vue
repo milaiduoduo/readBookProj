dc7e549bdd9b995870466106693897042d6a32d1
advancingKoala <milaiduoduo4567@163.com>
2019-12-09 15:53:02 +0800

    正式开发，删除不相关的文件，保留空框架

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,30 +1,11 @@
 <template>
-  <div class="wrap">
-    <div class="title">hello world</div>
-    <van-button type="primary" @click="clickHandle">按钮</van-button>
-  </div>
+  <div class="wrap"></div>
 </template>
 
 <script>
 import { get, post } from "@/utils/request.js";
-export default {
-  methods: {
-    clickHandle() {
-      console.log("1");
-      // this.$router.push("/pages/index/main");
-      post("https://www.baidu.com").then(response => {
-        console.log("post handle response...");
-      });
-    }
-  }
-};
+export default {};
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
-.wrap {
-  .title {
-    font-size: 40px;
-    color: #ff0000;
-  }
-}
 </style>
