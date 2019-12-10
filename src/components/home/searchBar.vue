5bfd449d69f42609ca8d37c126d75b11c4e2b77d
advancingKoala <milaiduoduo4567@163.com>
2019-12-10 11:26:31 +0800

    给搜索框组件新增了属性和事件，【】其中保留对onComfirm事件的调试

--- src/components/home/searchBar.vue
+++ src/components/home/searchBar.vue
@@ -2,15 +2,82 @@
   <div class="searchBarWrap">
     <div class="searchBar">
       <van-icon class="searchIcon icon" name="search" size="15px" />
-      <input class="searchInput" type="text" placeholder="搜索" />
-      <van-icon class="clearIcon icon" name="clear" size="15px" />
+      <input
+        class="searchInput"
+        type="number"
+        :focus="focus"
+        :disabled="disabled"
+        :maxlength="limit"
+        :placeholder="hotSearch"
+        v-model="searchWord"
+        @click="onSearchBarClick"
+        @input="onChange"
+        confirm-type="search"
+        @confirm="onConfirm"
+        placeholder-style="color:#adb4be"
+      />
+      <van-icon
+        class="clearIcon icon"
+        name="clear"
+        size="15px"
+        @click="onClearClick"
+        v-if="searchWord.length>0"
+      />
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 export default {
-  name: "m-search-bar"
+  name: "m-search-bar",
+  props: {
+    focus: {
+      type: Boolean,
+      default: true
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    limit: {
+      type: Number,
+      default: 50
+    },
+    hotSearch: {
+      type: String,
+      default: "搜索"
+    }
+  },
+  data() {
+    return {
+      searchWord: ""
+    };
+  },
+  methods: {
+    onSearchBarClick() {
+      this.$emit("onClick");
+    },
+    onClearClick() {
+      this.searchWord = "";
+      this.$emit("onClear");
+    },
+    onChange(e) {
+      console.log(e);
+      const { value } = e.mp.detail;
+      this.$emit("onChange", value);
+    },
+    onConfirm(e) {
+      console.log(e);
+      const { value } = e.mp.detail;
+      this.$emit("onConfirm", value);
+    },
+    setValue(v) {
+      this.searchWord = v;
+    },
+    getValue() {
+      return this.searchWord;
+    }
+  }
 };
 </script>
 
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="wrap">
-    <search-bar></search-bar>
+    <search-bar
+      :disabled="false"
+      @onClick="clickHandle"
+      @onClear="clearHandle"
+      @onChange="changeHandle"
+      @onConfirm="confirmHandle"
+    ></search-bar>
   </div>
 </template>
 
@@ -10,6 +16,20 @@ import searchBar from "@/components/home/searchBar.vue";
 export default {
   components: {
     searchBar
+  },
+  methods: {
+    clickHandle() {
+      console.log("父组件收到click");
+    },
+    clearHandle() {
+      console.log("父组件收到clear");
+    },
+    changeHandle(e) {
+      console.log("父组件收到change:", e);
+    },
+    confirmHandle(e) {
+      console.log("父组件收到confirm", e);
+    }
   }
 };
 </script>
